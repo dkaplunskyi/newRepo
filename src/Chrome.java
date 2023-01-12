cac90129416bab1c4beb67f9375cf2616626dddd
Dmytro Kaplunskyi <dkaplunsky@gmail.com>
2023-01-11 21:32:27 -0500

    edit to basic version

--- src/Chrome.java
+++ src/Chrome.java
@@ -1,8 +1,5 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 public class Chrome {
     public static void main(String[] args) throws InterruptedException {
@@ -10,15 +7,6 @@ public class Chrome {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://cyram.io");
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-//        js.executeScript("window.scrollBy(0, 1000)");
-        driver.findElement(By.tagName("footer"));
-        for (int i = 0; i < 50; i++) {
-            js.executeScript("window.scrollBy(0, 100)");
-            Thread.sleep(100);
-        }
-        Actions actions = new Actions(driver);
 
         Thread.sleep(2000);
         driver.quit();
